5cf38369f0dbcb04bea89c2231939a9b9cdda469
sina saniee <sinasan67@gmail.com>
2025-11-24 17:34:28 +0330

    add toggle

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import { Providers } from "@/components/Providers/ThemeProvider";
+import ThemeSwitcher from "@/components/ThemeSwich";
 
 export const metadata: Metadata = {
   title: "Dashboard Website",
@@ -15,7 +16,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-white dark:bg-gray-900 ">
-        <Providers>{children}</Providers>
+        <Providers>
+          <div className="absolute top-0 w-full bg-white dark:bg-gray-900 py-4 px-[5%] shadow-xl">
+            <ThemeSwitcher />
+          </div>
+          <div>{children}</div>
+        </Providers>
       </body>
     </html>
   );
